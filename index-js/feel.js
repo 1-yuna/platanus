2824f04db25932a895472a9a1c704831461001ac
1-yuna <97020423+1-yuna@users.noreply.github.com>
2022-05-09 21:33:19 +0900

    Create feel.js

---
+++ index-js/feel.js
@@ -0,0 +1,27 @@
+function feelbtnclick1(event) {
+  event.preventDefault();
+  const userfeel = "happy";
+  localStorage.setItem("savefeel", userfeel);
+  window.location.href = "main.html";
+}
+
+function feelbtnclick2(event) {
+  event.preventDefault();
+  const userfeel = "sad";
+  localStorage.setItem("savefeel", userfeel);
+  window.location.href = "main.html";
+}
+
+function feelbtnclick3(event) {
+  event.preventDefault();
+  const userfeel = "angry";
+  localStorage.setItem("savefeel", userfeel);
+  window.location.href = "main.html";
+}
+
+function feelbtnclick4(event) {
+  event.preventDefault();
+  const userfeel = "worry";
+  localStorage.setItem("savefeel", userfeel);
+  window.location.href = "main.html";
+}
