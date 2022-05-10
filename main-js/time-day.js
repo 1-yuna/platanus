1fff793ddb544c9483fe05e32354a48a79ce4720
1-yuna <97020423+1-yuna@users.noreply.github.com>
2022-05-10 18:52:04 +0900

    Update time-day.js

--- main-js/time-day.js
+++ main-js/time-day.js
@@ -1,4 +1,6 @@
 const clock = document.querySelector(".main_high-time-box p");
+const dayMonth = document.querySelector(".main_high-month");
+const dayDate = document.querySelector(".main_high-date");
 
 function getclock() {
   const date = new Date(); // 시간과 날짜 불러오기
@@ -6,7 +8,12 @@ function getclock() {
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
 
+  const monthNum = String(date.getMonth()).padStart(2, "0");
+  const dateNum = String(date.getDate()).padStart(2, "0");
+
   clock.innerText = `${hours} : ${minutes} : ${seconds}`;
+  dayMonth.innerText = monthNum;
+  dayDate.innerText = dateNum;
 }
 
 getclock(); // 1초 딜레이 방지
