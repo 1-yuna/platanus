d663ba319daf55f57e79fab7b856e5aa0b479763
1-yuna <97020423+1-yuna@users.noreply.github.com>
2022-05-11 23:06:19 +0900

    Update music-text.js

--- main-js/music-text.js
+++ main-js/music-text.js
@@ -31,8 +31,7 @@ if (getFeel === "happy") {
     musicTitle.innerText = "okay okay";
     musicText.innerText = "- Alessia cera";
     musicImg.src = "img/happy-1.jpg";
-    middleText.innerText =
-      "무슨 일이든 할 수 있다고 생각하는 사람이 해내는 법이다.";
+    middleText.innerText = "사람은 스스로 믿는 대로 된다.";
   }
 
   if (chooseMusicHappy === musicHappy[2]) {
@@ -63,5 +62,50 @@ if (getFeel === "happy") {
     musicText.innerText = "- Avril Lavigne";
     musicImg.src = "img/happy-5.jpg";
     middleText.innerText = "남들이 당신을 어떻게 생각할까 너무 걱정하지 말라.";
+  }
+}
+
+if (getFeel === "sad") {
+  const musicSad = ["Sad-0", "Sad-1", "Sad-2", "Sad-3", "Sad-4"];
+
+  const chooseMusicSad = musicSad[Math.floor(Math.random() * musicSad.length)];
+
+  console.log(chooseMusicSad);
+  musicPlay.src = `music/${chooseMusicSad}.mp3`;
+
+  if (chooseMusicSad === musicSad[0]) {
+    musicTitle.innerText = "무릎";
+    musicText.innerText = "- 아이유";
+    musicImg.src = "img/sad-0.jpg";
+    middleText.innerText = "아팠던 만큼 보다 더 반짝일 수 있는 그대이기를";
+  }
+
+  if (chooseMusicSad === musicSad[1]) {
+    musicTitle.innerText = "new normal";
+    musicText.innerText = "- khalid";
+    musicImg.src = "img/sad-1.jpg";
+    middleText.innerText = "희망은 절대 당신을 버리지 않는다.";
+  }
+
+  if (chooseMusicSad === musicSad[2]) {
+    musicTitle.innerText = "어른";
+    musicText.innerText = "- 손디아(sondia)";
+    musicImg.src = "img/sad-2.jpg";
+    middleText.innerText = "가장 소중한 것은 나 자신임을 잊지 말자";
+  }
+
+  if (chooseMusicSad === musicSad[3]) {
+    musicTitle.innerText = "밤하늘에";
+    musicText.innerText = "- 담소네공방";
+    musicImg.src = "img/sad-3.jpg";
+    middleText.innerText = "겨울이 오면 봄은 그리 멀지 않으리";
+  }
+
+  if (chooseMusicSad === musicSad[4]) {
+    musicTitle.innerText = "주저하는 연인들을 위해";
+    musicText.innerText = "- 잔나비";
+    musicImg.src = "img/sad-4.jpg";
+    middleText.innerText =
+      "빨리 도착하는 것이 목표가 아니다 힘들 땐 쉬어가라      ";
   }
 }
